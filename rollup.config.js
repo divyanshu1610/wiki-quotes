c9d05b31c4bc6bb762cfea8b60a3bffd779c2c49
Divyanshu Maurya <divyanshu1610@gmail.com>
2020-11-10 19:15:53 +0530

    rollup configured

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,31 @@
 export default {
   external: ['cheerio', 'node-fetch'],
   input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.common.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: 'dist/bundle.cjs.js',
+      format: 'cjs',
+      globals: {
+        cheerio: 'cheerio',
+        'node-fetch': 'fetch',
+      },
+    },
+    {
+      file: 'dist/bundle.esm.js',
+      format: 'esm',
+      globals: {
+        cheerio: 'cheerio',
+        'node-fetch': 'fetch',
+      },
+    },
+    {
+      name: 'Wikiquotes',
+      file: 'dist/bundle.umd.js',
+      format: 'umd',
+      globals: {
+        cheerio: 'cheerio',
+        'node-fetch': 'fetch',
+      },
+    },
+  ],
 }
